fc916286c41663d0fd5f16ada33ef91dab978d45
SemlanChalmers <31894663+SemlanChalmers@users.noreply.github.com>
2018-05-03 13:30:45 +0200

    New test

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,6 @@ import se.chalmers.cse.dat216.project.CartEvent;
 public class Main {
 
     public static void main(String[] args){
-
-
-
         
         Object t = new Object();
         CartEvent cartEvent = new CartEvent(t);
