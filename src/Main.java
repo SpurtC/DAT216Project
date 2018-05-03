4cee221cdc71e5c6b4079c2337259be992fe7091
Camilla Söderlund <camsoder@student.chalmers.se>
2018-05-03 09:25:48 +0200

    oj

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+
 import se.chalmers.cse.dat216.project.CartEvent;
 
 public class Main {
 
     public static void main(String[] args){
+
+
 
         Object t = new Object();
         CartEvent cartEvent = new CartEvent(t);
