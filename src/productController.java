dd2e6cad7be0871d48491ae2b77e65edf46e24dc
Camilla Söderlund <camsoder@student.chalmers.se>
2018-05-09 16:37:26 +0200

    kodpåbörjadpåriktigt

--- src/productController.java
+++ src/productController.java
@@ -1,9 +1,13 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import se.chalmers.cse.dat216.project.Product;
 
+import java.io.IOException;
 
 public class productController {
     @FXML private Text rubrikTxt, prisTxt;
@@ -11,9 +15,28 @@ public class productController {
     @FXML private Button minusBtn, plusBtn;
     @FXML private TextField antalTxtF;
 
+    public productController(Product product){
 
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("produkt.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
 
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
 
+        this.rubrikTxt.setText(product.getName());
+        this.prisTxt.setText(product.getPrice() + "");
+        this.varaImg.setImage(getFXImage(product));
+
+       
+    }
+
+    public Image getFXImage(Product product) {
+        return new Image("file:" + product.getImageName());
+    }
 
 
 
