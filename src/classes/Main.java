bc3021dd338d9a757d685d7511d8186e3b80f75f
SemlanChalmers <31894663+SemlanChalmers@users.noreply.github.com>
2018-05-14 15:48:52 +0200

    äsladknfäadfknlaknl

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application{
 
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/framsida.fxml"), bundle);
 
-            Scene scene = new Scene(root, 800, 500);
+            Scene scene = new Scene(root, 1440, 800);
 
             stage.setTitle(bundle.getString("application.name"));
             stage.setScene(scene);
