b546bc6c100eb219cdd3ae00a216c0d7aa52119b
SemlanChalmers <31894663+SemlanChalmers@users.noreply.github.com>
2018-05-26 14:21:33 +0200

    Bytt namn på klass

--- src/classes/MinHistorikController.java
+++ src/classes/MinHistorikController.java
@@ -11,7 +11,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-public class MinHistorik extends Controller {
+public class MinHistorikController extends Controller {
 
     @FXML
     private FlowPane historyFlowPane;
