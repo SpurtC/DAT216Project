45e6763e9cdf3cb757f21b42ff49a23d8dd25109
Camilla Söderlund <camsoder@student.chalmers.se>
2018-05-27 18:21:11 +0200

    CSS FUNGERAR

--- src/classes/CSSManager.java
+++ src/classes/CSSManager.java
@@ -2,17 +2,24 @@ package classes;
 
 import javafx.scene.layout.Pane;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CSSManager {
 
-    Map<String, Pane> stringPaneMap;
+    private Map<String, Pane> stringPaneMap;
+    private Map<Date, HistoryItem> dateHistoryItemMap;
 
     
 
     public CSSManager (Map<String, Pane> nameToPaneMap) {
         this.stringPaneMap = nameToPaneMap;
+    }
+
+    CSSManager (Map<Date, HistoryItem> dateToHistoryMap, String katt) {
+        this.dateHistoryItemMap = dateToHistoryMap;
     }
 
     public void changeCSS(String paneName, String notClickedCSSClass, String clickedOneCSSClass) {
@@ -22,6 +29,17 @@ public class CSSManager {
 
         }
         Pane pane = stringPaneMap.get(paneName);
+        pane.getStyleClass().clear();
+        pane.getStyleClass().add(clickedOneCSSClass);
+    }
+
+
+    public void changeCSSHistory (Date dateHistoryItem, String notClickedCSSClass, String clickedOneCSSClass) {
+        for(Pane pane : dateHistoryItemMap.values()) {
+            pane.getStyleClass().clear();
+            pane.getStyleClass().add(notClickedCSSClass);
+        }
+        Pane pane = dateHistoryItemMap.get(dateHistoryItem);
         pane.getStyleClass().clear();
         pane.getStyleClass().add(clickedOneCSSClass);
     }
